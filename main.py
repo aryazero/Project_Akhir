d5e592928e2c6b6f69c1f46507861c08266d172d
aryazero <156088830+aryazero@users.noreply.github.com>
2024-01-27 15:34:40 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import pickle
+import streamlit as st
+
+model = pickle.load(open('diabetes_model.sav', 'rb'))
+
+st.title('Prediksi Diabetes')
+
+Pregnancies = st.text_input ('Jumlah kehamilan')
+
+Glucose = st.text_input ('Kadar Gula darah (mg/dL)')
+
+BloodPressure = st.text_input ('Tekanan darah (mmHg)')
+
+SkinThickness = st.text_input ('Tebal Kulit')
+
+Insulin = st.text_input ('Kadar Insulin (mmol/L)')
+
+BMI = st.text_input ('Input nilai BMI')
+
+DiabetesPredigreeFunction = st.text_input ('Input Nilai DPF')
+
+Age = st.text_input ('Input Usia')
+
+diab_diagnosis = ''
+
+if st.button('Prediksi Diabetes') :
+    diab_prediction = model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPredigreeFunction, Age]])
+
+    if(diab_prediction[0] == 1):
+        diab_diagnosis = 'Pasien terkena Diabet'
+    else :
+        diab_diagnosis = 'Pasien tidak terkena Diabet'
+    
+    st.success(diab_diagnosis)
+
